6d2619ac5b28179cabdbb8f6ad8aa8b20a9ddf79
Xabier <xaberortega@gmail.com>
2024-06-24 15:02:25 +0200

    ex05

--- 1/ex05/main.cpp
+++ 1/ex05/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-#include "placeholder.hpp"
+#include "Harl.hpp"
 
 
-int main() {
-    
-    return 0;
+int main(int argc, char *argv[])
+{
+	Harl Harl;
+
+	if (argc == 2)
+		Harl.complain(argv[1]);
+	else
+		std::cout << "PLS I DONT FUNCTION UNLES U PASS ME 1 ARGUMENT" << std::endl;
+
+    return (1);
 }
