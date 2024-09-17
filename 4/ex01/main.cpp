f9762f82b8d366c12110a1a090f379a8286c25ff
Xabier Ortega <xortega@c3r7s5.42urduliz.com>
2024-09-17 11:40:56 +0200

    cpp4 ex02 done i think

--- 4/ex01/main.cpp
+++ 4/ex01/main.cpp
@@ -2,32 +2,29 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 #include "Animal.hpp"
-#include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
 
 
 int main()
 {
     
-const Animal		*meta = new Animal();
-const Animal		*j = new Dog();
-const Animal		*i = new Cat();
-const WrongAnimal	*k = new WrongCat();
+const Animal		*animals[100];
 
+for (size_t i = 0; i < 100; i++)
+{
+	if (i % 2)
+    	animals[i] = new Cat();
+	else
+		animals[i] = new Dog();
+}
 
+for (size_t i = 0; i < 100; i++)
+	std::cout << animals[i]->get_type() << " " << std::endl;
 
-std::cout << j->get_type() << " " << std::endl;
-std::cout << i->get_type() << " " << std::endl;
-std::cout << k->get_type() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-k->makeSound();
-meta->makeSound();
+for (size_t i = 0; i < 100; i++)
+	animals[i]->makeSound();
 
-delete meta;
-delete j;
-delete i;
-delete k;
+for (size_t i = 0; i < 100; i++)
+    	delete animals[i];
 
 return 0;
 }
