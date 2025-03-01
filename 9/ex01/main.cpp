0acf8f2ea83cf5fb8728a1a0e4f2c43f3f422c25
Xabier <xaberortega@gmail.com>
2025-03-01 13:52:11 +0100

    cpp9 ex02 done whit bools

--- 9/ex01/main.cpp
+++ 9/ex01/main.cpp
@@ -2,7 +2,16 @@
 #include "RPN.hpp"
 
 
-int main() {
-    
+int main(int argc, char **argv) {
+
+    if (argc != 2)
+    {
+        std::cout << "Wrong number of arguments" << std::endl;
+        return(1);
+    }
+
+	std::string line(argv[1]);
+    if (!RPN::calculate(line))
+        std::cout << "Error" << std::endl;
     return 0;
 }
