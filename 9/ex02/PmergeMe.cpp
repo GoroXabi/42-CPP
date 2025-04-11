2aa2edfa5e851749ff974242b72f31dd974b35f3
Xabier Ortega <xaberortega@gmail.com>
2025-04-11 15:16:20 +0200

    pmergeme is really working, wow

--- 9/ex02/PmergeMe.cpp
+++ 9/ex02/PmergeMe.cpp
@@ -89,33 +89,6 @@ std::list<std::list<int> > unite(std::list<std::list<int> > secuence)
 	return(new_secuence);
 }
 
-std::list<int>::iterator aait(std::list<int> &list, size_t idx)
-{
-	if (idx > list.size())
-		return(list.end());
-	if (idx == 0)
-		return(list.begin());
-	
-	std::list<int>::iterator it = list.begin();
-	for (size_t i = 0; i < idx; i++)
-		it++;
-
-	return(it);
-}
-std::list<std::list<int> >::iterator aait(std::list<std::list<int> > &list, size_t idx)
-{
-	if (idx > list.size())
-		return(list.end());
-	if (idx == 0)
-		return(list.begin());
-	
-	std::list<std::list<int> >::iterator it = list.begin();
-	for (size_t i = 0; i < idx; i++)
-		it++;
-
-	return(it);
-}
-
 std::list<int> splitList(std::list<int> &to_split)
 {
 	std::list<int> new_list;
@@ -138,58 +111,159 @@ std::list<std::list<int> > divide(std::list<std::list<int> > secuence)
 	return(new_secuence);
 }
 
-bool is_shorted(std::vector<std::vector<int> > secuence)
+bool is_shorted(std::list<std::list<int> > secuence)
 {
-	for(std::vector<std::vector<int> >::iterator it = secuence.begin(); it != secuence.end() - 1; it++)
-		if (it->back() > (it + 1)->back())
+	for(std::list<std::list<int> >::iterator it = secuence.begin(); it != secuence.end(); it++)
+	{
+		std::list<std::list<int> >::iterator tmp = it;
+		tmp++;
+		if (tmp == secuence.end())
+			break;
+		if (it->back() > (tmp)->back())
 			return(false);
+	}
 	return(true);
 
 }
 
-int nextJacobsthal(int n_jacobsthal)
-{
-	return(((pow(2, n_jacobsthal)) - (pow(-1, n_jacobsthal))) / 3);
-}
+#define nextJacobsthal(n_jacobsthal) ((pow(2, n_jacobsthal)) - (pow(-1, n_jacobsthal))) / 3
 
-std::list<std::list<int> > insertePendIntoMain(std::list<std::list<int> > main, std::list<std::list<int> > pend)
+std::list<std::list<int> > insertePendIntoMain(
+	std::list<std::list<int> > &main,
+	std::list<std::list<int> > &pend,
+	std::list<std::list<std::list<int> >::iterator> &it_list)
 {
 	int n_jacobsthal = 2;
-	int jacobsthal;
+	size_t jacobsthal;
 
-	jacobsthal = nextJacobsthal(n_jacobsthal + 1) - nextJacobsthal(n_jacobsthal);
-	jacobsthal++;
 
-	while (pend.size() > 0)
+	while (!pend.empty())
 	{
-		std::list<std::list<int> >::iterator to_sort = aait(pend, jacobsthal);
+		jacobsthal = nextJacobsthal(n_jacobsthal + 1) - nextJacobsthal(n_jacobsthal);
+		
+		if (jacobsthal > it_list.size())
+			jacobsthal = it_list.size() - 1;
+		if (it_list.size() == 1)
+			jacobsthal = 1;
+
+		int tmp_jacobsthal = jacobsthal;
+ 		while(tmp_jacobsthal > 0)
+		{		
+			std::list<std::list<int> >::iterator bN = aait(pend, tmp_jacobsthal - 1);
+			std::list<std::list<int> >::iterator aN = *aait(it_list, tmp_jacobsthal - 1);
+
+			for (; aN != main.begin();)
+			{
+				if (aN->back() < bN->back())
+				{
+					aN++;
+					main.insert(aN, *bN);
+					break;
+				}
+				--aN;
+				if (aN == main.begin())
+				{
+					if (aN->back() < bN->back())
+					{
+						aN++;
+						main.insert(aN, *bN);
+					}
+					else
+						main.push_front(*bN);
+					break;
+				}
+			}
+			tmp_jacobsthal--;
+		}
+
+		for (size_t i = 0; i < jacobsthal; i++)
+		{
+			it_list.pop_front();
+			pend.pop_front();
+		}
+		n_jacobsthal++;
 	}
+	return(main);
 	
 }
 
-void insert(std::list<std::list<int> > secuence)
+void prueba(std::list<std::list<std::list<int> >::iterator> it_list, size_t n)
+{
+	std::cout << it_list.size() << std::endl;
+	std::list<std::list<std::list<int> >::iterator>::iterator it;
+	if (it_list.size() == 0 || n == 0)
+		return;
+	if (n > it_list.size())
+		it = aait(it_list, it_list.size() - 1);
+	else
+		it = aait(it_list, n - 1);
+	for (; it != it_list.begin(); --it)
+	{
+		std::list<std::list<int> >::iterator tmp = *it;
+		std::cout << tmp->back() << " ";
+	}
+	std::list<std::list<int> >::iterator tmp = *it;
+	std::cout << tmp->back() << " ";
+	std::cout << std::endl;
+	
+}
+
+void printaN(std::list<std::list<int> > secuence, std::list<std::list<std::list<int> >::iterator> it_list)
+{
+	print_secuence(secuence);
+
+	if (it_list.size() > 0)
+	{
+		std::cout << "last digit of all aN: ";
+		for (std::list<std::list<std::list<int> >::iterator>::iterator it = it_list.begin(); it != it_list.end(); it++)
+		{
+			std::list<std::list<int> >::iterator tmp = *it;
+			std::cout << tmp->back() << " ";
+
+		}	
+		std::cout << std::endl; 
+	}
+	else
+		std::cout << "no ";
+
+
+}
+void insert(std::list<std::list<int> > &secuence)
 {
 	std::list<std::list<int> > pend;
 	std::list<std::list<std::list<int> >::iterator> it_list;
-	int noRandomAccess = 0;
+	int noRandomAccess = 3;
 
-	for (std::list<std::list<int> >::iterator it = secuence.begin(); it != secuence.end(); it++)
+	for (std::list<std::list<int> >::iterator it = aait(secuence, 3); it != secuence.end(); it++)
 	{
 		if(noRandomAccess % 2)
-			it_list.push_back(it);
+			it_list.push_back(aait(secuence, noRandomAccess));
 		noRandomAccess++;		
 	}
+	if (secuence.size() % 2)
+	{
+		it_list.push_back(it_list.back());
+	}
+
+	std::cout << "--------------------" << std::endl;
+	std::cout << "before: ";
+	print_secuence(secuence);
+
 
     size_t list_size = secuence.size();
     for (size_t i = 2; i < list_size; i++)
 	{
         pend.splice(pend.end(), secuence, aait(secuence, i), aait(secuence, i + 1));
 	}
-	
+
 	std::cout << "pend: ";
 	print_secuence(pend);
 	std::cout << "main: ";
 	print_secuence(secuence);
+	secuence = insertePendIntoMain(secuence, pend, it_list);
+	std::cout << "after insert: ";
+	print_secuence(secuence);
+	
 
 
 
@@ -225,18 +299,18 @@ std::list<std::list<int> > calculate(std::list<std::list<int> > secuence)
 void PmergeMe::calculateList()
 {
 	std::list<std::list<int> > secuence;
-	std::list<int> init;
+	//std::list<int> init;
  
-/* 	size_t count = 40;
-	for (size_t i = 0; i < count; i++)
+	srand(time(NULL));
+	for (size_t i = 0; i < 10000; i++)
 	{
 		std::list<int> init;
-		init.push_back(rand() % 100);
+		init.push_back(rand() % 10000);
 		secuence.push_back(init);
-	} */
+	}
 
 	//11 2 17 0 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7
-	init.push_back(11);
+/* 	init.push_back(11);
 	init.push_back(2);
 	init.push_back(17);
 	init.push_back(0);
@@ -257,14 +331,15 @@ void PmergeMe::calculateList()
 	init.push_back(4);
 	init.push_back(20);
 	init.push_back(13);
-	init.push_back(7);
-
+	init.push_back(7); */
+/* 
 	for (std::list<int>::iterator it = init.begin(); it != init.end(); it++)
 	{
 		std::list<int>v;
 		v.push_back(*it);
 		secuence.push_back(v);
-	}
+	} */
 	secuence = calculate(secuence);
+	std::cout << (is_shorted(secuence) ? "IS SHORTED" : "FUCK") << std::endl;
 	//print_secuence(secuence);
 }
--- 9/ex02/PmergeMe.hpp
+++ 9/ex02/PmergeMe.hpp
@@ -12,6 +12,7 @@
 #include <time.h>
 #include <algorithm>
 #include <cmath>
+#include <iterator>
 
 #define HRED	"\033[91m"
 #define HBLU	"\033[94m"
@@ -44,7 +45,21 @@ public:
 };
 
 void print_secuence(std::list<int> secuence);
-std::list<int>::iterator aait(std::list<int> &list, size_t idx);
+
+template <typename T>
+typename T::iterator aait(T &list, size_t idx)
+{
+	if (idx > list.size())
+		return(list.end());
+	if (idx == 0)
+		return(list.begin());
+	
+	typename T::iterator it = list.begin();
+	for (size_t i = 0; i < idx; i++)
+		it++;
+
+	return(it);
+}
 
 
 #endif
